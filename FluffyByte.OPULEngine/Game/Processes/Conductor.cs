b57d41f76d573c0a0233b2f4b15de6f298b56dcb
fluffybyte <philosopherjake@msn.com>
2025-09-30 02:03:39 -0600

    Built in support for the Conductor.

---
+++ FluffyByte.OPULEngine/Game/Processes/Conductor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluffyByte.OPULEngine.Startup;
+
+namespace FluffyByte.OPULEngine.Game.Processes;
+
+public sealed class Conductor : FluffyCoreProcess
+{
+    public override string Name => "Game Conductor";
+
+    protected override async Task OnStart()
+    {
+        await Task.CompletedTask;
+    }
+
+    protected override async Task OnStop()
+    {
+        await Task.CompletedTask;
+    }
+
+}
---
+++ FluffyByte.OPULEngine/Startup/ServerOperator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluffyByte.OPULEngine.Startup;
+
+public sealed class ServerOperator : FluffyCoreProcess
+{
+    private static readonly Lazy<ServerOperator> _instance = new(() => new ServerOperator());
+    public static ServerOperator Instance => _instance.Value;
+
+    public override string Name => "Server Operator";
+    
+    protected override async Task OnStart()
+    {
+        await Task.CompletedTask;
+    }
+
+    protected override async Task OnStop()
+    {
+        await Task.CompletedTask;
+    }
+
+
+}
